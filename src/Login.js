27a3d74a5843d468508418b932ec160e8769722f
Sheikh Antor <antoralways2025@gmail.comm>
2026-02-27 07:56:51 +0600

    connect to live backend

--- src/Login.js
+++ src/Login.js
@@ -10,7 +10,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:5000/api/login", {
+      const response = await fetch("https://f-backend-fun.onrender.com/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
